ef5328ee0b9a08733122752992432746a06fb673
yoyoyonoyo <you.itwork@gmail.com>
2020-07-08 09:45:04 +0900

    create JS & install jQuery & update view
    JS作成
    jQuery導入
    フォーム作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  before_action :set_product, except: [:index, :new, :create]
 
   def index
     @products = Product.includes(:images).order('created_at DESC')
@@ -15,7 +16,6 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-    binding.pry
     if @product.save
       redirect_to root_path
     else
@@ -27,15 +27,23 @@ class ProductsController < ApplicationController
   end
 
   def update
+    if @product.update(product_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   def destroy
   end
 
   private
-    def product_params
-    params.require(:product).permit(:name, :price, :category_id, :description, :brand, :size_id, :status_id, :shipfee_id, :shipregion_id, :estshipdate_id, :seller_id, :buyer_id, images_attributes: [:src]).merge(seller_id: 1)
+  def product_params
+    params.require(:product).permit(:name, :price, :category_id, :description, :brand, :size_id, :status_id, :shipfee_id, :shipregion_id, :estshipdate_id, :seller_id, :buyer_id, images_attributes: [:src, :_destroy, :id]).merge(seller_id: 1)
   end
 
+  def set_product
+    @product = Product.find(params[:id])
+  end
 
 end
