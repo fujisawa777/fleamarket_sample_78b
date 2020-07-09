13e5116a7dd0f291cad53db9b21afd595610af7b
athlaliel <athla8452@gmail.com>
2020-07-09 20:46:30 +0900

    update2 product_spec.rb
    7月８日の終了時点のモデルのテスト完了。
    コントロールはまだこれから。

---
+++ spec/factories/products.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+
+  factory :product do
+    name                  {"test"}
+    price                 {1000}
+    category_id              {1}
+    description           {"setumei"}
+    brand                 {"viton"}
+    size_id               {1}
+    status_id             {2}
+    shipfee_id            {2}
+    shipregion_id         {1}
+    estshipdate_id        {1}
+  end
+
+end
--- spec/model/product_spec.rb
+++ spec/model/product_spec.rb
@@ -2,47 +2,68 @@ require 'rails_helper'
 
 describe Product do
   describe '#create' do
-    it "not name" do
-      product = Product.new(name: "", price: "1000", category_id: 1, description: "setumei", status_id: 1, shipfee_id: 1, shipregion_id: 2, estshipdate_id: 4, seller_id: 1, images_attributes: {"0"=>{"src"=>"hoge.jpg"}})
+    it "is invalid without a name" do
+      product = build(:product, name: "")
       product.valid?
 
       expect(product.errors[:name]).to include("can't be blank")
     end
 
-    it "not price" do
-      product = Product.new(name: "tanaka", price: "", category_id: 1, description: "setumei", status_id: 1, shipfee_id: 1, shipregion_id: 2, estshipdate_id: 4, seller_id: 1, images_attributes: {"0"=>{"src"=>"hoge.jpg"}})
+    it "is invalid without a price" do
+      product = build(:product, price: "")
       product.valid?
 
       expect(product.errors[:price]).to include("can't be blank")
     end
 
-    it "not category" do
-      product = Product.new(name: "tanaka", price: "1000", category_id: "", description: "setumei", status_id: 1, shipfee_id: 1, shipregion_id: 2, estshipdate_id: 4, seller_id: 1, images_attributes: {"0"=>{"src"=>"hoge.jpg"}})
+    it "is invalid without a category_id" do
+      product = build(:product, category_id: "")
       product.valid?
 
       expect(product.errors[:category_id]).to include("can't be blank")
       expect(product.errors[:category]).to include("must exist")
     end
 
-    it "not description" do
-      product = Product.new(name: "tanaka", price: "1000", category_id: 1, description: "", status_id: 1, shipfee_id: 1, shipregion_id: 2, estshipdate_id: 4, seller_id: 1, images_attributes: {"0"=>{"src"=>"hoge.jpg"}})
+    it "is invalid without a description" do
+      product = build(:product, description: "")
       product.valid?
 
       expect(product.errors[:description]).to include("can't be blank")
     end
 
-    it "not status" do
-      product = Product.new(name: "tanaka", price: "1000", category_id: 1, description: "setumei", status_id: "", shipfee_id: 1, shipregion_id: 2, estshipdate_id: 4, seller_id: 1, images_attributes: {"0"=>{"src"=>"hoge.jpg"}})
+    # it "is valid without a brand" do
+    #   product = build(:product, brand: "")
+    #   product.valid?
+
+    #   expect(product.errors[:description]).to include("can't be blank")
+    # end
+
+    it "is invalid without a status_id" do
+      product = build(:product, status_id: "")
       product.valid?
       
       expect(product.errors[:status_id]).to include("can't be blank")
     end
 
-    it "not shipfee" do
-      product = Product.new(name: "tanaka", price: "1000", category_id: 1, description: "setumei", status_id: 1, shipfee_id: "", shipregion_id: 2, estshipdate_id: 4, seller_id: 1, images_attributes: {"0"=>{"src"=>"hoge.jpg"}})
+    it "is invalid without a shipfee_id" do
+      product = build(:product, shipfee_id: "")
       product.valid?
       
       expect(product.errors[:shipfee_id]).to include("can't be blank")
+    end
+
+    it "is invalid without a shipregion_id" do
+      product = build(:product, shipregion_id: "")
+      product.valid?
+      
+      expect(product.errors[:shipregion_id]).to include("can't be blank")
+    end
+
+    it "is invalid without a estshipdate_id" do
+      product = build(:product, estshipdate_id: "")
+      product.valid?
+      
+      expect(product.errors[:estshipdate_id]).to include("can't be blank")
     end
   end
 end
