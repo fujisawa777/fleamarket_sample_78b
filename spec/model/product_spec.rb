fd6283cee74cb82ad2a6090d8d7ee733078f605d
athlaliel <athla8452@gmail.com>
2020-07-09 17:40:07 +0900

    update product_spec.rb
    プロダクトのテスト更新

--- spec/model/product_spec.rb
+++ spec/model/product_spec.rb
@@ -23,5 +23,26 @@ describe Product do
       expect(product.errors[:category_id]).to include("can't be blank")
       expect(product.errors[:category]).to include("must exist")
     end
+
+    it "not description" do
+      product = Product.new(name: "tanaka", price: "1000", category_id: 1, description: "", status_id: 1, shipfee_id: 1, shipregion_id: 2, estshipdate_id: 4, seller_id: 1, images_attributes: {"0"=>{"src"=>"hoge.jpg"}})
+      product.valid?
+
+      expect(product.errors[:description]).to include("can't be blank")
+    end
+
+    it "not status" do
+      product = Product.new(name: "tanaka", price: "1000", category_id: 1, description: "setumei", status_id: "", shipfee_id: 1, shipregion_id: 2, estshipdate_id: 4, seller_id: 1, images_attributes: {"0"=>{"src"=>"hoge.jpg"}})
+      product.valid?
+      
+      expect(product.errors[:status_id]).to include("can't be blank")
+    end
+
+    it "not shipfee" do
+      product = Product.new(name: "tanaka", price: "1000", category_id: 1, description: "setumei", status_id: 1, shipfee_id: "", shipregion_id: 2, estshipdate_id: 4, seller_id: 1, images_attributes: {"0"=>{"src"=>"hoge.jpg"}})
+      product.valid?
+      
+      expect(product.errors[:shipfee_id]).to include("can't be blank")
+    end
   end
 end
