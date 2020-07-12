188253e43aa88f88484d512a26a19d24cc3c6984
fujisawa777 <66346129+fujisawa777@users.noreply.github.com>
2020-07-12 13:17:35 +0900

    update header

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ApplicationRecord
     has_many :products
     has_closure_tree
 
-    validates :name, presence: true, uniqueness: true
+    #validates :name, presence: true, uniqueness: true
 end
