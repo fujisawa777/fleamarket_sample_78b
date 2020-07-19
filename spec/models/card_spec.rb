71cefd8daa50289514ae4e8cbffc947b43bc81a9
fujisawa777 <66346129+fujisawa777@users.noreply.github.com>
2020-07-19 12:54:31 +0900

    update card_spec.rb
    カードモデルのテスト作成

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe Card, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Card do
+
+  describe '#create_card' do
+
+    it "customer_idが空かどうか" do
+      card = build(:card, customer_id: "")
+      card.valid?
+      expect(card.errors[:customer_id]).to include("を入力してください")
+    end
+
+    it "card_idが空かどうか" do
+      card = build(:card, card_id: "")
+      card.valid?
+      expect(card.errors[:card_id]).to include("を入力してください")
+    end
+
+  end
+
 end
